928a432cd6ffcea4c3962fc6f409ea18a367099c
José Arévalo <joseaf1002@gmail.com>
2023-04-21 20:20:16 +0200

    Create project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Header } from './components/Header/Header';
+import { ProductList } from './components/ProductList/ProductList';
+import { SearchBar } from './components/SearchBar/SearchBar';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <SearchBar />
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import App from './App';
+
+import './index.css';
+import { store } from './app/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
